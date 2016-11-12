3bcd9cb28f3c3d32bc55ff21716401ef6f7501cc
noname2241 <ezhlovbogdan@gmail.com>
2016-11-12 19:45:23 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  devServer : {
+    host: 'localhost',
+    port: 2222,
+    hot: true,
+    contentBase: __dirname + '/src/client'
+  }, 
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        exlude: [/node_module/, /public/],
+        loader : 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
